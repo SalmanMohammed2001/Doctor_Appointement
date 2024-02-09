82d80768522aabffa50d456a6585357932c490ab
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-02-10 02:40:40 +0530

    header style add:

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,8 @@
 import logo from '../../assets/images/logo.png'
-import {NavLink} from "react-router-dom";
-
+import {Link, NavLink} from "react-router-dom";
+import userImg from '../../assets/images/avatar-icon.png'
+import {BiMenu} from 'react-icons/bi'
+import {useRef} from "react";
 
 const navLink = [
     {
@@ -21,6 +23,9 @@ const navLink = [
     },
 ]
 const Header = () => {
+
+
+
     return (
         <header className="header flex items-center">
             <div className="container">
@@ -29,7 +34,7 @@ const Header = () => {
                     <div>
                         <img src={logo} alt=""/>
                     </div>
-
+                    {/*============menu==============================*/}
                     <div className="navihation ">
                         <ul className="menu flex items-center gap-[2.7rem]">
                             {navLink.map((link, index) => {
@@ -37,7 +42,7 @@ const Header = () => {
                                     <li key={index}>
                                         <NavLink to={link.path}
                                                  className={navClass => navClass.isActive ? "text-primaryColor text-[16px] leading-7" +
-                                                     "font-[600]" : "text-textColor text-[16px] leading-7 font-[500]"}>{link.display}</NavLink>
+                                                     "font-[600]" : "text-textColor text-[16px] leading-7 font-[500] hover:text-primaryColor"}>{link.display}</NavLink>
                                     </li>
                                 )
                             })
@@ -46,8 +51,27 @@ const Header = () => {
                         </ul>
                     </div>
 
-                </div>
+                    {/*==================nav right===================*/}
 
+                <div className={"flex items-center gap-4"}>
+                    <div className={"hidden"}>
+                        <Link to={"/"}>
+                            <figure className="w-[35px] h-[35px] rounded-full cursor-pointer">
+                                <img src={userImg} className="w-full rounded-full" alt=""/>
+                            </figure>
+                        </Link>
+                    </div>
+                    <Link to={"/login"}>
+                        <button className="bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center
+                        justify-center rounded-[50px]">
+                            Login
+                        </button>
+                    </Link>
+                    <span className={"md:hidden"}>
+                        <BiMenu className={"w-6 h-6 cursor-pointer"}/>
+                    </span>
+                </div>
+                </div>
             </div>
         </header>
     );
