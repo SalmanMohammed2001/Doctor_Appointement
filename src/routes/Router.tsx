45da8923bb7b40beef18b8b3c05865450b00e94a
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-02-10 01:06:30 +0530

    add routes

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,8 +1,25 @@
+import {Routes,Route} from "react-router-dom"
+import Home from "../pages/Home.tsx";
+import Doctors from "../pages/Doctors/Doctors.tsx";
+import DoctorsDetails from "../pages/Doctors/DoctorsDetails.tsx";
+import Login from "../pages/Login.tsx";
+import Signup from "../pages/Signup.tsx";
+import Contact from "../pages/Contact.tsx";
+import Service from "../pages/Service.tsx";
 
 const Router = () => {
     return (
         <div>
-            <h1>Routers</h1>
+            <Routes>
+                <Route path={"/"} element={<Home/>}/>
+                <Route path={"/home"} element={<Home/>}/>
+                <Route path={"/doctors"} element={<Doctors/>}/>
+                <Route path={"/doctors/:id"} element={<DoctorsDetails/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+                <Route path={"/register"} element={<Signup/>}/>
+                <Route path={"/contact"} element={<Contact/>}/>
+                <Route path={"/service"} element={<Service/>}/>
+            </Routes>
         </div>
     );
 };
