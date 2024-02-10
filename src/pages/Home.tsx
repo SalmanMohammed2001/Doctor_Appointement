5e4a7aa358d5718238aa2f370f09f17e64c78cbb
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-02-10 17:44:34 +0530

    header hero content ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
-
-
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            {/*===============Hero section=========================================*/}
+            <section className="hero_section  border-2 border-solid  border-green-800 pt-[60px] 2xl:h-[800px]">
+                <div className="container">
+                    <div
+                        className="flex flex-col  lg:flex-row gap-[90px] items-center justify-between border-2 border-solid border-blue-800 ">
+                        {/*=============hero content============*/}
+                        <div>
+                            <div className="lg:w-[570px] border-2 border-solid border-amber-300 ">
+                                <h1 className="text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px] md:leading-[70px]">We
+                                    help patient live a healthy ,longer life</h1>
+
+                                <p className="text_para">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi blanditiis corporis
+                                    ea eveniet, excepturi fugiat harum hic impedit labore mollitia nam nisi porro quia
+                                    reiciendis rem repellendus, similique sint soluta?
+                                </p>
+                                <button className={"btn"}> Request an Appointment</button>
+                            </div>
+                            {/*=============hero content============*/}
+                            <div className="mt-[30px] lg:mt-[70px] flex flex-col lg:flex-row lg:items-center"></div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </section>
         </div>
     );
 };
